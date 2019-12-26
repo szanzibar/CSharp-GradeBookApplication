23588c2db0ca88f26a3bc3727bb0dc39490aac4b
Steven van Dijk <Steven.vanDijk@dev.adminarsenal.net>
2019-12-26 15:27:27 -0700

    finish module 7: weighted grading

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,10 +14,12 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
+            IsWeighted = isWeighted;
             Students = new List<Student>();
         }
 
@@ -107,18 +109,23 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int baseGPA = 0;
+            if (IsWeighted == true && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+            {
+                baseGPA = 1;
+            }
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 4 + baseGPA;
                 case 'B':
-                    return 3;
+                    return 3 + baseGPA;
                 case 'C':
-                    return 2;
+                    return 2 + baseGPA;
                 case 'D':
-                    return 1;
+                    return 1 + baseGPA;
                 case 'F':
-                    return 0;
+                    return 0 + baseGPA;
             }
             return 0;
         }
